420e7841d3dd6734df7afbf66ed3e1c9cb99927a
juan-s <juan.naranjo@dreamcodesoft.com>
2021-08-10 23:37:06 -0500

    valiendo v

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -20,11 +22,9 @@ import "slick-carousel/slick/slick-theme.css";
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    
-       <App />
-    
-  </React.StrictMode>,
+  <BrowserRouter>
+  <App />
+</BrowserRouter>,
   document.getElementById('root')
 );
 
