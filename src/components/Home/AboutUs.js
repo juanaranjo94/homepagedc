7f4538323c8c1955a79c7f43e1550434a8ce64a8
Juan Naranjo <juan.naranjo@dreamcodesoft.com>
2023-02-07 12:47:50 -0500

    nuevo cambio

--- src/components/Home/AboutUs.js
+++ src/components/Home/AboutUs.js
@@ -16,7 +16,7 @@ class AboutUs extends Component{
                         </div>
                         <div className="col-lg-6">
                               <div className="seo_title">
-                                <SeoTitle Title={t(this.props.about_home_title)} Tclass='text-left'/>                               
+                                <SeoTitle Title="Titulo" Tclass='text-left'/>                               
                                 <p >{t(this.props.about_home_d)}</p>
                                 <a href="/About" className="learn_btn">{t(this.props.read_more)}<i className="ti-arrow-right"></i></a>
                                
